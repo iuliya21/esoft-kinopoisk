ea01084d102648005054d843b452285c4cc35615
iuliya21 <iuliya21@mail.ru>
2024-05-02 21:48:29 +0300

    feat/routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { useFilmsStore } from "../../services/store";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -5,6 +6,9 @@ import Main from "../Main/Main";
 import Sidebar from "../Sidebar/Sidebar";
 import styles from "./App.module.css";
 import { useEffect } from "react";
+import FilmDetails from "../../pages/FilmDetails/FilmDetails";
+import NotFound from "../../pages/NotFound/NotFound";
+import SearchPage from "../../pages/SearchPage/SearchPage";
 
 function App() {
   const { fetchFilms } = useFilmsStore();
@@ -19,7 +23,12 @@ function App() {
       <div className={styles.content}>
         <div>
           <Header />
-          <Main />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/film/:idFilm" element={<FilmDetails />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
         <Footer />
       </div>
---
+++ src/pages/FilmDetails/FilmDetails.jsx
@@ -0,0 +1,9 @@
+function FilmDetails() {
+  return (
+    <div>
+      123
+    </div>
+  );
+}
+
+export default FilmDetails;
