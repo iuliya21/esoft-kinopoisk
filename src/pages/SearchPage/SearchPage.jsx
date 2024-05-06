7a84e1d747c37a3b6a3e284fd254f4241da304b2
iuliya21 <iuliya21@mail.ru>
2024-05-06 21:51:52 +0300

    feat/Header navigation

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,19 @@
+import styles from "./SearchPage.module.css";
+
 function SearchPage() {
   return (
-    <div>
-      SearchPage
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <legend className={styles.title}>Поиск фильма по параметрам</legend>
+        <div className={styles.inputs}>
+          <label htmlFor="">Название фильма</label>
+          <input type="text" placeholder="блабла" />
+        </div>
+        <div className={styles.inputs}>
+          <label htmlFor="">Название фильма</label>
+          <input type="text" />
+        </div>
+      </form>
     </div>
   );
 }
